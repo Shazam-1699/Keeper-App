7d55e476627cf6e9a42f3a8e0d9fbb4b52335876
Shazam-1699 <Shazam1699@gmail.com>
2024-12-25 22:53:35 +0500

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+var date = new Date();
+const currentYear = date.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright © {currentYear}</p>
+    </footer>
+  );
+}
+export default Footer;
